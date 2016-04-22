94addb315ac3cb0bb91a801e509fdb10ea14ef7e
Luka Gado <gadoschi@gmail.com>
2016-04-22 17:10:20 +0200

    Dodan link za sve postove

--- admin.php
+++ admin.php
@@ -16,38 +16,9 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	 <?php 
- 	// Izlistanje postova i brisanje istih
- 	$sql = "SELECT * FROM mrznja.postovi ORDER BY id DESC";
- 	$result = mysqli_query($db, $sql);
- 	if(!$result){
- 		echo 'Greška pri dohvaćanju svih podataka!';
- 	}else{
- 		while($row = mysqli_fetch_assoc($result)){
- 			echo $row['post'];
- 			echo "<br>";
- 			echo '<form action="admin.php" method="post" accept-charset="utf-8">
- 				  <input type="hidden" value="'.$row['id'].'" name="id">
- 				  <input type="submit" value="Obriši" name="delete">
- 				  </form>';
- 		}
- 	}
 
-	if(isset($_POST['delete'])){
- 		$id = $_POST['id'];
- 		echo $id;
- 		echo 'tu';
- 		$sql = "DELETE FROM mrznja.postovi WHERE id = $id";
- 		$result = mysqli_query($db, $sql);
- 		if(!$result){
- 			echo 'ne radi';
- 		}else{
- 			echo 'Uspješno ste obrisali oglas '. $id;
-			header("Refresh:2;url=admin.php");
- 		}
- 	}
-
-  ?>
+	<a href="allposts.php">Sve objave</a><br>
+	 
 </body>
 </html>
 
