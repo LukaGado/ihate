be29d9b744d1a668df244f25ec754d0601cc35d6
Stike999 <stike999@gmail.com>
2016-04-23 14:16:17 +0200

    index
    index ovca

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ include 'includes/db.php';
     </script>
  </head>
  <body>
-  <nav class="navbar navbar-default navbar-fixed-top">
+  <nav class="navbar navbar-default navbar-fixed-top col-md-offset-1 col-md-8">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -64,30 +64,34 @@ include 'includes/db.php';
   </div><!-- /.container-fluid -->
 </nav>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-offset-1 col-md-10 wrapper">
+<!-- <div class="container">
+    <div class="row"> -->
+        <div class="col-md-offset-1 col-md-8 wrapper">
+          <div class="index-left col-md-offset-1 col-md-7">
             <form action="index.php" method="POST" charset="utf-8">
-              <div class="form-group col-xs-4">
+              <div class="form-group col-xs-6">
                 <label class="control-label col-sm-2" for="username">Nadimak:</label><br> 
                 <input class="form-control" type="text" id="username" name="username"></div>
-             <div class="form-group col-xs-5">
+             <div class="form-group col-xs-10">
                 <label class="control-label col-sm-2" for="post">Post:</label><br>
                 <textarea class="form-control" rows="3" maxlength="500" name="post" id="post" onkeyup="countChar(this)"></textarea></div>
-              <div class="control-label col-sm-4">
+              <div class="control-label col-sm-5">
                 <p style="float:left;">Preostalo znakova:&nbsp</p>
                 <div id="charNum">500</div></div>
-             <div class="form-group col-xs-2">
+             <div class="form-group col-xs-4">
                 <label class="control-label" for="vercode">Unesite kod:</label> 
                 <img src="includes/captcha.php" class="nowidth"> &nbsp <input class="form-control" type="text" name="vercode" style="margin-top:10px;" /></div><br>
                 <button class="btn btn-hate btn-lg" type="submit" name="submit"><span class="glyphicon glyphicon-fire"></span> Mrzi!</button>
-                <div class="right-image">
+                </form>
+              </div> <!-- end index-left -->
+                <div class="right-image col-md-5">
                   <img src="img/angry-sheep.png" />
                 </div>
-            </form>    
+
+                <div style="clear:both;"></div>          
         </div>
-    </div><!-- end row -->
-</div><!-- end container -->
+   <!-- </div> end row -->
+<!--</div> end container -->
 
  </body>
  </html>
