dfee62206dc80b6f24d906cd600f6a52826213b2
Stike999 <stike999@gmail.com>
2016-04-23 18:04:22 +0200

    update obavijesti
    update obavijesti

--- includes/objava.php
+++ includes/objava.php
@@ -5,15 +5,22 @@ if(isset($_POST['submit'])){
 			$post = mysqli_real_escape_string($db, $_POST['post']);
 			$broj = 0;
  			if ($_POST["vercode"] != $_SESSION["vercode"] OR $_SESSION["vercode"]=='' OR $_POST['post'] == '')  { 
-				echo  '<div class="alert alert-danger" role="alert">Morate popuniti sva polja!</div>';
-				header("Refresh:2;url=index.php");
+				echo  '<div class="alert alert-danger alert-dismissible fade in col-md-offset-1 col-md-8" role="alert">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				    <span aria-hidden="true">&times;</span>
+				 </button>
+  					<p>Morate popuniti sva polja!</p></div>';
 				}else{ 	
 					if($username == ''){
 						$username = 'Anonimni ljuti lik';
 					}
 					if(strlen($post) > 500){
-						echo  '<div class="alert alert-danger" role="alert">Post predugacak! Maksimalan broj znakova je 500!</div>';
-						header("Refresh:2;url=index.php");
+						echo  '<div class="alert alert-danger alert-dismissible fade in col-md-offset-1 col-md-8" role="alert">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				    <span aria-hidden="true">&times;</span>
+				 </button>
+  					<p>Post predugačak. Maksimalan broj znakova je 500.</p></div>';
+					
 					}else{
 						$sql = "INSERT INTO  `mrznja`.`postovi` (`id` ,`username` ,`post` ,`broj_pregleda` ,`datum`)VALUES (NULL ,  '$username',  '$post',  '$broj', NOW())";
 
@@ -21,7 +28,11 @@ if(isset($_POST['submit'])){
 					if(!$result){
 						echo 'Došlo je do pogreške!';
 					}else{
-						echo '<div class="alert alert-success" role="alert">Uspješno ste objavili post!</div>';
+						echo '<div class="alert alert-success alert-dismissible fade in col-md-offset-1 col-md-8" role="alert">
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				    <span aria-hidden="true">&times;</span>
+				 </button>
+  					<p><strong>Uspjeh!</strong>&nbspObjavili ste post!</p></div>';
 						header("Refresh:2;url=index.php");
 					}
 					}
